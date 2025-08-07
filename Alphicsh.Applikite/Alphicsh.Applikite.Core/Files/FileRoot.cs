019c045d2bff04150ecd365074af762b7bc0049c
Alice Jankowska <alphish.creature@gmail.com>
2025-08-07 09:20:01 +0200

    ISS-1: Ported over file path and filesystem

---
+++ Alphicsh.Applikite/Alphicsh.Applikite.Core/Files/FileRoot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using IOPath = System.IO.Path;
+
+namespace Alphicsh.Applikite.Files;
+
+public static class FileRoot
+{
+    public static string Location { get; } =
+        IOPath.GetPathRoot(typeof(FileRoot).Assembly.Location)?.NormalizePath()
+        ?? throw new NotSupportedException("Could not determine a reference file root.");
+
+    public static FilePath Path { get; } = new FilePath(Location);
+}
+
