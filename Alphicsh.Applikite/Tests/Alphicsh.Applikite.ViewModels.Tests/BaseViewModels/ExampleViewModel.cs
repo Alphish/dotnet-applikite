24a348281c872c9de72669839b552cf587ecef6b
Alice Jankowska <alphish.creature@gmail.com>
2025-08-11 17:50:56 +0200

    ISS-3: Added tests for the base view model

---
+++ Alphicsh.Applikite/Tests/Alphicsh.Applikite.ViewModels.Tests/BaseViewModels/ExampleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Alphicsh.Applikite.ViewModels.Properties;
+
+namespace Alphicsh.Applikite.ViewModels.Tests.BaseViewModels;
+
+public class ExampleViewModel : BaseViewModel
+{
+    public ExampleViewModel(ExampleModel model)
+    {
+        Model = model;
+        ExampleValueProperty = RelayProperty(nameof(ExampleValue), Model.ExampleValueSource);
+    }
+
+    public ExampleModel Model { get; }
+
+    public RelayViewModelProperty<int> ExampleValueProperty { get; }
+    public int ExampleValue { get => ExampleValueProperty.Value; set => ExampleValueProperty.Value = value; }
+}
