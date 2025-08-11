a7fb9113d733de3acabbd1430d0adc457156264d
Alice Jankowska <alphish.creature@gmail.com>
2025-08-11 19:10:08 +0200

    ISS-3: Carried over plain and conditional commands

---
+++ Alphicsh.Applikite/Library/Alphicsh.Applikite.ViewModels/Commands/PlainCommand.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Input;
+
+namespace Alphicsh.Applikite.ViewModels.Commands;
+
+public class PlainCommand : ICommand
+{
+    private Action ExecutionAction { get; }
+
+    // --------
+    // Creation
+    // --------
+
+    public PlainCommand(Action executionAction)
+    {
+        ExecutionAction = executionAction;
+    }
+
+    // -----------------------
+    // ICommand implementation
+    // -----------------------
+
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public void Execute(object? parameter)
+    {
+        ExecutionAction();
+    }
+}
