596e7aed00ad5d185edb9228dcb1d9ab99b2a2fc
Alice Jankowska <alphish.creature@gmail.com>
2025-08-19 19:38:58 +0200

    ISS-2: Carried over and tweaked the progress management portion of tasks module

---
+++ Alphicsh.Applikite/Alphicsh.Applikite.Core/Tasks/Progress/IProgressManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Alphicsh.Applikite.Tasks.Progress;
+
+public interface IProgressManager : IProgress<object>
+{
+    event EventHandler<object>? ProgressChanged;
+    void SetSender(object sender);
+    void Cancel();
+    void Link(IProgress<object> manager);
+}
---
+++ Alphicsh.Applikite/Alphicsh.Applikite.Core/Tasks/Progress/ProgressHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Alphicsh.Applikite.Tasks.Progress;
+
+public class ProgressHandler<TProgress> : IProgressHandler<TProgress>
+{
+    private IProgressManager SourceProgress { get; }
+    public event EventHandler<TProgress>? ProgressChanged;
+
+    public ProgressHandler(IProgressManager sourceProgress)
+    {
+        SourceProgress = sourceProgress;
+        SourceProgress.ProgressChanged += (sender, e) =>
+        {
+            if (e is not TProgress typedProgress)
+                return;
+
+            ProgressChanged?.Invoke(sender, typedProgress);
+        };
+    }
+}
