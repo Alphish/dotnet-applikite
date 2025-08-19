fa54774f9269b7a90f6eb442d897641b6eb54f3c
Alice Jankowska <alphish.creature@gmail.com>
2025-08-19 19:40:58 +0200

    ISS-2: Created utilities for managed task sources to generate new tasks

---
+++ Alphicsh.Applikite/Alphicsh.Applikite.Core/Tasks/Sources/BaseTaskSource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Alphicsh.Applikite.Tasks.Progress;
+
+namespace Alphicsh.Applikite.Tasks.Sources;
+
+public abstract class BaseTaskSource<TResult> : ITaskSource<TResult>
+{
+    public IManagedTask<TResult> Create()
+    {
+        var cancellationSource = new CancellationTokenSource();
+        var progress = new ProgressManager();
+        var task = Run(cancellationSource.Token, progress);
+        return new ManagedTask<TResult>(cancellationSource, progress, task);
+    }
+
+    public abstract Task<TResult> Run(CancellationToken cancellationToken, IProgress<object> progress);
+}
