deca76f7d875c96097172a7df1395613c0da1287
Alice Jankowska <alphish.creature@gmail.com>
2025-08-25 08:54:49 +0200

    ISS-2: Carried over the integer progress

---
+++ Alphicsh.Applikite/Alphicsh.Applikite.Core/Tasks/Progress/IProgress.cs
@@ -0,0 +1,6 @@
+﻿namespace Alphicsh.Applikite.Tasks.Progress;
+
+public interface IProgressData<TProgress> where TProgress : IProgressData<TProgress>
+{
+    static abstract TProgress CreateDefault();
+}
---
+++ Alphicsh.Applikite/Alphicsh.Applikite.Core/Tasks/Progress/IntegerProgress.cs
@@ -0,0 +1,10 @@
+﻿namespace Alphicsh.Applikite.Tasks.Progress;
+
+public struct IntegerProgress : IProgressData<IntegerProgress>
+{
+    public required int Current { get; init; }
+    public required int Target { get; init; }
+
+    public static IntegerProgress CreateDefault()
+        => new IntegerProgress { Current = 0, Target = 1 };
+}
