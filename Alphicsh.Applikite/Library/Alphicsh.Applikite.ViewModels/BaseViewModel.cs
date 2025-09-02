0f597a27556e0c7009cc6e5bc0416902e83f4d4a
Alice Jankowska <alphish.creature@gmail.com>
2025-09-02 23:30:00 +0200

    ISS-4: Disabled implicit global usings on the ViewModels project

--- Alphicsh.Applikite/Library/Alphicsh.Applikite.ViewModels/BaseViewModel.cs
+++ Alphicsh.Applikite/Library/Alphicsh.Applikite.ViewModels/BaseViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using Alphicsh.Applikite.Models;
 using Alphicsh.Applikite.ViewModels.Properties;
 
